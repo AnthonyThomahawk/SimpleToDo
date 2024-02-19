e7ef4fa28918476bf5c3fd2d750745fd1d728722
Anthony T. <antonis301@gmail.com>
2024-02-19 22:51:31 +0200

    Cleaned up remove functionality, now it exists as a context menu item

--- src/main/java/org/simpleToDo/MainPanel.java
+++ src/main/java/org/simpleToDo/MainPanel.java
@@ -8,6 +8,7 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 
 /**
@@ -61,6 +62,25 @@ public class MainPanel extends JPanel {
 
     public void addTask(String content) {
         JCheckBox newTask = new JCheckBox(content);
+        newTask.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                if (e.getButton() == MouseEvent.BUTTON3) {
+                    JPopupMenu contextmenu = new JPopupMenu();
+                    JMenuItem deleteAction = new JMenuItem("Remove");
+                    deleteAction.addActionListener(new AbstractAction() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            removeTask(newTask);
+                        }
+                    });
+
+                    contextmenu.add(deleteAction);
+                    contextmenu.show(e.getComponent(), e.getX(), e.getY());
+                }
+            }
+        });
         contentP.add(Box.createRigidArea(new Dimension(5, 5)));
         contentP.add(newTask);
         scrollPane1.setViewportView(contentP);
@@ -83,28 +103,12 @@ public class MainPanel extends JPanel {
         d.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
     }
 
-    private void button2(ActionEvent e) {
-        JDialog d = new JDialog(Main.mainWindow, "Remove tasks...", true);
-        d.getContentPane().add(new RemoveTasks());
-        d.pack();
-        d.setLocationRelativeTo(null);
-        d.setVisible(true);
-        d.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-        d.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosed(WindowEvent e) {
-                super.windowClosed(e);
-                refreshViewport();
-            }
-        });
+    private void thisFocusGained(FocusEvent e) {
+        // TODO add your code here
+    }
 
-        d.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowDeactivated(WindowEvent e) {
-                super.windowDeactivated(e);
-                refreshViewport();
-            }
-        });
+    private void scrollPane1FocusGained(FocusEvent e) {
+        // TODO add your code here
     }
 
     private void initComponents() {
@@ -113,21 +117,32 @@ public class MainPanel extends JPanel {
         label1 = new JLabel();
         scrollPane1 = new JScrollPane();
         button1 = new JButton();
-        button2 = new JButton();
 
         //======== this ========
+        addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                thisFocusGained(e);
+            }
+        });
 
         //---- label1 ----
         label1.setText("Tasks");
         label1.setFont(label1.getFont().deriveFont(label1.getFont().getStyle() | Font.BOLD, label1.getFont().getSize() + 14f));
 
+        //======== scrollPane1 ========
+        {
+            scrollPane1.addFocusListener(new FocusAdapter() {
+                @Override
+                public void focusGained(FocusEvent e) {
+                    scrollPane1FocusGained(e);
+                }
+            });
+        }
+
         //---- button1 ----
         button1.setText("Add a task \u2795");
         button1.addActionListener(e -> button1(e));
-
-        //---- button2 ----
-        button2.setText("Remove tasks \u2796");
-        button2.addActionListener(e -> button2(e));
 
         GroupLayout layout = new GroupLayout(this);
         setLayout(layout);
@@ -139,9 +154,7 @@ public class MainPanel extends JPanel {
                         .addComponent(scrollPane1)
                         .addGroup(layout.createSequentialGroup()
                             .addComponent(label1)
-                            .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 116, Short.MAX_VALUE)
-                            .addComponent(button2, GroupLayout.PREFERRED_SIZE, 126, GroupLayout.PREFERRED_SIZE)
-                            .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                            .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 248, Short.MAX_VALUE)
                             .addComponent(button1, GroupLayout.PREFERRED_SIZE, 129, GroupLayout.PREFERRED_SIZE)))
                     .addContainerGap())
         );
@@ -151,7 +164,6 @@ public class MainPanel extends JPanel {
                     .addContainerGap()
                     .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING, false)
                         .addComponent(label1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(button2, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(button1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                     .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(scrollPane1, GroupLayout.DEFAULT_SIZE, 311, Short.MAX_VALUE)
@@ -165,7 +177,6 @@ public class MainPanel extends JPanel {
     private JLabel label1;
     private JScrollPane scrollPane1;
     private JButton button1;
-    private JButton button2;
     // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
 
     private BoxLayout taskLayout;
